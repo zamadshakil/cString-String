01314c3ab90c2a1ea39f26b2413ed858a292c00b
zamadshakil <146313727+zamadshakil@users.noreply.github.com>
2025-01-23 04:33:10 +0500

    Len and to Comp. Cstring

--- cString-String.cpp
+++ cString-String.cpp
@@ -7,5 +7,10 @@ int main() {
 	char str[] = "Hello\n";
 	char str2[] = " World";
 
+
+	cout << " " << strlen(str) << " " << endl;
+	cout << " " << strlen(str2) << " ";
+	//check if both are same or not
+	(strcmp(str, str2) == 0) ? cout << "Same Strings" : cout << "Not Same";
 	return 0;
 }

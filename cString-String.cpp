f2e8a89bb25ff13c9be512cec44c2a004ddf54e2
zamadshakil <146313727+zamadshakil@users.noreply.github.com>
2025-01-23 05:43:35 +0500

    .

--- cString-String.cpp
+++ cString-String.cpp
@@ -4,13 +4,28 @@
 using namespace std;
 
 int main() {
-	char str[] = "Hello\n";
+	char dest[120] = "Hello\n";
 	char str2[] = " World";
 
 
-	cout << " " << strlen(str) << " " << endl;
+	cout << " " << strlen(dest) << " " << endl;
 	cout << " " << strlen(str2) << " ";
 	//check if both are same or not
-	(strcmp(str, str2) == 0) ? cout << "Same Strings" : cout << "Not Same";
+	(strcmp(dest, str2) == 0) ? cout << "Same Strings" : cout << "Not Same";
+	
+	// Concatenation of two cStrings
+	//strncat_s(dest, str2, 12);
+
+	//copy c string
+	//strcpy_s(dest, str2);
+	//cout << dest;
+
+	//copy particular lenght of characters, 
+	strncpy_s(dest, str2, 3);
+	cout << dest;
+	
+
+
 	return 0;
+
 }
